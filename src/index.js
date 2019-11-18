d68a4774416132fd8222b4db9fc23ba0d7236ecd
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-11-18 12:19:08 -0600

    Complex state with useState

--- src/index.js
+++ src/index.js
@@ -2,22 +2,61 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      { title, body }
+    ]);
+    setTitle("");
+    setBody("");
+  }
 
-const App = ({ initialCount = 0 }) => {
-  const [ count, setCount ] = useState(initialCount);
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title))
+  }
 
   return (
     <div>
-      <p>The current count is { count }</p>
-      <button onClick={() => setCount(count + 1)} >+1</button>
-      <button onClick={() => setCount(count - 1)}> -1</button>
-      <button onClick={() => setCount(initialCount)}>reset</button> 
+      <h1>Notes</h1>
+      {notes.map(note => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <button type="submit">add note</button>
+      </form>
     </div>
   )
 }
 
-ReactDOM.render(<App initialCount={10}/>, document.getElementById('root'));
+const App = ({ initialCount = 0 }) => {
+  const [ count, setCount ] = useState(initialCount);
+  const [ text, setText ] = useState('');
+
+  return (
+    <div>
+      <p>The current { text || 'count' } is { count }</p>
+      <button onClick={() => setCount(count + 1)} >+1</button>
+      <button onClick={() => setCount(count - 1)}> -1</button>
+      <button onClick={() => setCount(initialCount)}>reset</button> 
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<NoteApp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
