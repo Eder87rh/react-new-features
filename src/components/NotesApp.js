7aa7fd88af259f7ae1a3861cb3584769b1024c7e
Eder Ramirez <eder.ramirez87@gmail.com>
2019-11-21 09:11:21 -0600

    Context API

--- src/components/NotesApp.js
+++ src/components/NotesApp.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState, useReducer} from 'react';
+import React, {useEffect, useReducer} from 'react';
 import notesReducer from '../reducers/notes';
 import NoteList from './NoteList';
 import AddNoteForm from './AddNoteForm';
-
+import NotesContext from '../context/notes-context';
 const NoteApp = () => {
   const [notes, dispatch] = useReducer(notesReducer, []);
 
@@ -18,17 +18,15 @@ const NoteApp = () => {
     localStorage.setItem("notes", JSON.stringify(notes))
   }, [notes]);
 
-  const removeNote = (title) => {
-    dispatch({ type: 'REMOVE_NOTE', title })
-  }
+  
 
   return (
-    <div>
+    <NotesContext.Provider value={{ notes, dispatch }}>
       <h1>Notes</h1>
-      <NoteList notes={notes} removeNote={removeNote} />
-      <AddNoteForm dispatch={dispatch} />
+      <NoteList />
+      <AddNoteForm />
       
-    </div>
+    </NotesContext.Provider>
   )
 }
 
